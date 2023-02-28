ebdbb3c487f400e1a794c3a2572eed27afe8e5e3
Yazzi <1066209603@qq.com>
2023-02-28 22:57:26 +0800

    sidemenu end p167

--- src/components/SideMenu/SideMenu.js
+++ src/components/SideMenu/SideMenu.js
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from "react";
-import { Menu,Layout } from "antd";
-import {
-  SettingOutlined,
-} from "@ant-design/icons";
+import { Menu, Layout } from "antd";
+import { SettingOutlined } from "@ant-design/icons";
 import "./SideMenu.css";
 import "antd/dist/reset.css";
 import { withRouter } from "react-router-dom";
@@ -10,44 +8,14 @@ import axios from "axios";
 const { SubMenu } = Menu;
 const { Sider } = Layout;
 
-const menuList = [
-  {
-    key: "/home",
-    title: "首页",
-    icon: <SettingOutlined />,
-  },
-  {
-    key: "/user-manage",
-    title: "用户管理",
-    icon: <SettingOutlined />,
-    children: [
-      {
-        key: "/user-manage/list",
-        title: "用户列表",
-        icon: <SettingOutlined />,
-      },
-    ],
-  },
-  {
-    key: "/right-manage",
-    title: "权限管理",
-    icon: <SettingOutlined />,
-    children: [
-      {
-        key: "/right-manage/role/list",
-        title: "角色列表",
-        icon: <SettingOutlined />,
-      },
-      {
-        key: "/right-manage/right/list",
-        title: "权限列表",
-        icon: <SettingOutlined />,
-      },
-    ],
-  },
-];
-
-// submenu keys of first level
+const iconList = {
+  "/home": <SettingOutlined />,
+  "/user-manage": <SettingOutlined />,
+  "/user-manage/list": <SettingOutlined />,
+  "/right-manage": <SettingOutlined />,
+  "/right-manage/role/list": <SettingOutlined />,
+  "/right-manage/right/list": <SettingOutlined />,
+};
 
 function SideMenu(props) {
   const [menu, setMenu] = useState([]);
@@ -59,35 +27,58 @@ function SideMenu(props) {
     });
   }, []);
 
+  const checkPagePermisson = (item) => {
+    return item.pagepermisson;
+  };
+
   const renderMenu = (menuList) => {
     return menuList.map((item) => {
-      if (item.children) {
+      if (
+        item.children &&
+        checkPagePermisson(item) &&
+        item.children.length > 1
+      ) {
         return (
-          <SubMenu key={item.key} icon={item.icon} title={item.title}>
+          <SubMenu key={item.key} icon={iconList[item.key]} title={item.title}>
             {renderMenu(item.children)}
           </SubMenu>
         );
       }
       return (
-        <Menu.Item
-          key={item.key}
-          icon={item.icon}
-          onClick={() => {
-            props.history.push(item.key);
-          }}
-        >
-          {item.title}
-        </Menu.Item>
+        checkPagePermisson(item) && (
+          <Menu.Item
+            key={item.key}
+            icon={iconList[item.key]}
+            onClick={() => {
+              props.history.push(item.key);
+            }}
+          >
+            {item.title}
+          </Menu.Item>
+        )
       );
     });
   };
 
+  // console.log(props.location.pathname.split("/")[1]);
+  const openKeys = ["/" + props.location.pathname.split("/")[1]];
+  const selectKeys = [props.location.pathname];
+
   return (
     <Sider trigger={null} collapsible collapsed={false}>
-      <div className="logo">News</div>
-      <Menu theme="dark" mode="inline" defaultSelectedKeys={["3"]}>
-        {renderMenu(menuList)}
-      </Menu>
+      <div style={{ display: "flex", height: "100%", flexDirection: "column" }}>
+        <div className="logo">News</div>
+        <div style={{ flex: 1, overflow: "auto" }}>
+          <Menu
+            theme="dark"
+            mode="inline"
+            selectedKeys={selectKeys}
+            defaultOpenKeys={openKeys}
+          >
+            {renderMenu(menu)}
+          </Menu>
+        </div>
+      </div>
     </Sider>
   );
 }
